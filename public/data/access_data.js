c0574f0a0bbfa2ca887614475d67fc9f6f34a825
Phasma12-wq <148236232+Phasma12-wq@users.noreply.github.com>
2025-05-07 18:44:39 +0200

    File Access Logic added (Work-in-Progress)

--- public/data/access_data.js
+++ public/data/access_data.js
@@ -1,21 +1,65 @@
 const fs = require('fs')
 
-events = []
-contacts = []
-team_info = []
+//Arrays for data access
+var about = [];
+var events = [];
+var contacts = [];
 
+//functions to load data onto arrays
 function getEvents() {
-    
+    fs.readFile('events.json' , 'utf-8', (err, data) => {
+        if (err) {
+            console.error('Error reading the file: ', err);
+            return;
+        }
+
+        const jsonData = JSON.parse(data);
+
+        return jsonData
+    });
 }
 
 function getAbout() {
+    about
+    
+    fs.readFile('about.json' , 'utf-8', (err, data) => {
+        if (err) {
+            console.error('Error reading the file: ', err);
+            return;
+        }
 
+        const jsonData = JSON.parse(data);
+
+        return jsonData
+    });
+}
+
+function writeToAbout(name, surname, phone){
+    const contact = {
+        firstName: name,
+        lastName: surname,
+        Phone: phone
+    }
+
+    const jsonData = JSON.stringify(contact, null, 4)
+
+    fs.writeFile('about.json', jsonData, 'utf-8', (err) => {
+        if (err) {
+            console.error('Error writing to file: ', err);
+            return;
+        }
+    });
 }
 
 function getContact() {
+    fs.readFile('contact.json' , 'utf-8', (err, data) => {
+        if (err) {
+            console.error('Error reading the file: ', err);
+            return;
+        }
 
-}
+        const jsonData = JSON.parse(data);
 
-function writeContacts() {
-
+        return jsonData
+    });
 }
