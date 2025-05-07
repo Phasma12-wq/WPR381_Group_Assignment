e79dc5e989f75e66bdabba850dfdb1388d74a2b7
Phasma12-wq <148236232+Phasma12-wq@users.noreply.github.com>
2025-05-07 17:27:34 +0200

    data acces js file added

---
+++ public/data/access_data.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+
+events = []
+contacts = []
+team_info = []
+
+function getEvents() {
+    
+}
+
+function getAbout() {
+
+}
+
+function getContact() {
+
+}
+
+function writeContacts() {
+
+}
