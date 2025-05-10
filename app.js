9929dd66453f5cf027af3d40a4b0d308c7c13051
Phasma12-wq <dawbe.mapre@gmail.com>
2025-05-11 00:19:39 +0200

    Additional Middleware

--- app.js
+++ app.js
@@ -8,14 +8,30 @@ const pageRoutes = require("./routes/pageRoutes");
 const app = express();
 const port = 3000;
 
-//used to allow the server to accept form data
+const loggingMiddleware = (req, res, next) => {
+  console.log(`[${new Date()}] ${req.method} ${req.url}`);
+  next();
+};
+
+app.use(loggingMiddleware)
+
+//Middleware to allow the server to accept form data
 // and parse JSON data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+
+const errorHandler = (err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
+};
+
+app.use(errorHandler)
+
 
 app.use("/", pageRoutes);
 
