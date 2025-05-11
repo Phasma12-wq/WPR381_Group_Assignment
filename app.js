2409e07edcd1cdf529c59e4bfab727019ba5170b
Phasma12-wq <dawbe.mapre@gmail.com>
2025-05-11 09:51:41 +0200

    Middleware + redirect code

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ app.use(loggingMiddleware)
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
